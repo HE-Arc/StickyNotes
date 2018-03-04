1319ccc15bd4c3e95521a1e6e5b4d4a41d94d5ed
Julia Németh <cptnwing@gmail.com>
2018-03-04 20:17:17 +0100

    Minor fixes
    Fixed errors occured after the merge

--- stickynotes/notes/urls.py
+++ stickynotes/notes/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    url(r'^$', views.home),
+    url(r'^notes', views.notes, name='view_notes'),
     path('create', views.create, name='create_stickynotes'),
     path('delete/<int:id_stickynote>', views.delete, name='delete_stickynotes'),
     path('update/<int:id_stickynote>', views.update, name='update_stickynotes'),
--- stickynotes/notes/views.py
+++ stickynotes/notes/views.py
@@ -3,7 +3,10 @@ from notes.models import StickyNote
 from notes.forms import StickyNoteForm
 
 # Create your views here.
-def NotesPage(request):
+def home(request):
+    return render(request, 'home.html')
+
+def notes(request):
     stickynotes = StickyNote.objects.all()
     return render(request, 'notes/note.html', {'stickynotes' : stickynotes})
 
@@ -15,7 +18,7 @@ def create(request):
         save_it = form.save(commit=False)
         save_it.user_created = request.user
         save_it.save()
-        return redirect(home)
+        return redirect(notes)
     return render(request, 'notes/note_create_form.html', {'stickynotes' : stickynotes, 'form' : form})
 
 def update(request, id_stickynote):
@@ -23,10 +26,10 @@ def update(request, id_stickynote):
     form = StickyNoteForm(request.POST or None, instance=stickynote)
     if form.is_valid():
         form.save()
-        return redirect(home)
+        return redirect(notes)
     return render(request, "notes/note_create_form.html", {'form': form})
 
 def delete(request, id_stickynote):
     stickynote = StickyNote.objects.get(id=id_stickynote)
     stickynote.delete()
-    return redirect(home)
+    return redirect(notes)
