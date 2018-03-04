c22fe4ce41ddb3022c47a75a59529e3161266d39
Julia Németh <cptnwing@gmail.com>
2018-03-04 21:28:51 +0100

    Login/logout
    basic implementation, next step -> add modals

--- stickynotes/notes/views.py
+++ stickynotes/notes/views.py
@@ -18,7 +18,7 @@ def create(request):
         save_it = form.save(commit=False)
         save_it.user_created = request.user
         save_it.save()
-        return redirect(notes)
+        return redirect(home)
     return render(request, 'notes/note_create_form.html', {'stickynotes' : stickynotes, 'form' : form})
 
 def update(request, id_stickynote):
@@ -26,10 +26,10 @@ def update(request, id_stickynote):
     form = StickyNoteForm(request.POST or None, instance=stickynote)
     if form.is_valid():
         form.save()
-        return redirect(notes)
+        return redirect(home)
     return render(request, "notes/note_create_form.html", {'form': form})
 
 def delete(request, id_stickynote):
     stickynote = StickyNote.objects.get(id=id_stickynote)
     stickynote.delete()
-    return redirect(notes)
+    return redirect(home)
