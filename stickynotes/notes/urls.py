edb214b3d24ac28da90c36835f535d05c1180e67
Julia Németh <cptnwing@gmail.com>
2018-03-05 16:53:58 +0100

    Register
    with basic User

--- stickynotes/notes/urls.py
+++ stickynotes/notes/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home),
     url(r'^notes', views.notes, name='view_notes'),
+    path('register', views.Register.as_view(), name='register'),
     path('create/<str:type_stickynote>', views.create, name='create_stickynotes'),
     path('delete/<int:id_stickynote>', views.delete, name='delete_stickynotes'),
     path('update/<int:id_stickynote>', views.update, name='update_stickynotes'),
