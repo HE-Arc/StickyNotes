4027874813b917278f987bde3f29a17935af6fbb
Thomas Lovis <thomas.lovis@gmail.com>
2018-04-20 12:57:52 +0200

    correct bug

--- stickynotes/notes/views.py
+++ stickynotes/notes/views.py
@@ -114,9 +114,9 @@ def delete_stickynotes(request, id_chalkboard, id_stickynote):
 def join_chalkboard(request, id_chalkboard):
     chlk = get_object_or_404(Chalkboard, id=id_chalkboard)
     user_join = JoinChalkboard.objects.get(chalkboard=chlk, user_id=request.user)
-    if chlk.is_private and not user_join:
-        return redirect('own_chalkboard')
-    elif chlk.is_private:
+#    if chlk.is_private and not user_join:
+#        return redirect('own_chalkboard')
+    if chlk.is_private:
         return redirect('details_chalkboard', id_chalkboard)
     # default permision (CRUD own notes)
     assign_default_permission_join_chalkboard(request.user, chlk)
